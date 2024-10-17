ab7f0c256c5cba7f324ecfbbd96d4224fe6f792c
Moslaoui <mohamedslaoui777@gmail.com>
2024-10-17 18:40:28 +0100

    init project

---
+++ consumer.py
@@ -0,0 +1,46 @@
+import pika
+import subprocess
+import os
+
+# Define the RabbitMQ broker URL
+credentials = pika.PlainCredentials('user', 'password')
+parameters = pika.ConnectionParameters('rabbitmqservername.eastus.azurecontainer.io', 5672, '/', credentials)
+
+# Establish a connection to RabbitMQ server
+connection = pika.BlockingConnection(parameters)
+channel = connection.channel()
+
+# Declare the same fanout exchange
+channel.exchange_declare(exchange='pubsub_exchange', exchange_type='fanout')
+
+# Create a temporary queue and bind it to the exchange
+result = channel.queue_declare(queue='', exclusive=True)
+queue_name = result.method.queue
+
+channel.queue_bind(exchange='pubsub_exchange', queue=queue_name)
+
+print(' [*] Waiting for messages. To exit press CTRL+C')
+
+# Define a callback function to process incoming messages
+def callback(ch, method, properties, body):
+    print(f" [x] Received script content")
+    
+    # Write the received script content to a file
+    script_path = "/app/received_script.sh"
+    with open(script_path, 'wb') as script_file:
+        script_file.write(body)
+    
+    # Set execute permissions for the script
+    os.chmod(script_path, 0o755)
+    
+    # Execute the script on the host OS using docker exec
+    try:
+        subprocess.run(["docker", "exec", "-i", "rabbitmq-consumer-container", "sh", script_path], check=True)
+        print(f" [x] Executed script on host OS: {script_path}")
+    except subprocess.CalledProcessError as e:
+        print(f" [x] Failed to execute script on host OS: {e}")
+
+# Start consuming messages
+channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
+
+channel.start_consuming()
